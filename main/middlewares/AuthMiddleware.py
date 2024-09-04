9bc185c7df17ac7704f2a406744b6d65236a0cec
Mustafa Gülsoy <mustafagulsoy05@gmail.com>
2024-09-04 14:11:39 +0300

    Changing Requiremet fileChanging Requiremet file

--- main/middlewares/AuthMiddleware.py
+++ main/middlewares/AuthMiddleware.py
@@ -11,8 +11,8 @@ class AuthMiddleware:
     def __call__(self, request):
         # URL of the external API
 
-        api_url = os.getenv('AUTH_IP', 'fallback-ip') + ':30028/gateway/auth/api/v1/Session/isActive'
-        # api_url = 'https://pacs.konyasm.gov.tr:30028/gateway/auth/api/v1/Session/isActive'
+        # api_url = os.getenv('AUTH_IP', 'fallback-ip') + 'https://pacs.konyasm.gov.tr:30028/gateway/auth/api/v1/Session/isActive'
+        api_url = 'https://pacs.konyasm.gov.tr:30028/gateway/auth/api/v1/Session/isActive'
         # 'https://pacs.konyasm.gov.tr:30028/gateway/auth/api/v1/Token'
         authorization_header = request.META.get('HTTP_AUTHORIZATION')
         # Headers for the API request (if needed)
